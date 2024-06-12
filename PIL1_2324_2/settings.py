8dc75fbaf90dda88d55cdf120fc56a12c4401d16
Rosas Behoundja <perrierosas@gmail.com>
2024-06-13 00:14:48 +0100

    Récupération de mot de passe

--- PIL1_2324_2/settings.py
+++ PIL1_2324_2/settings.py
@@ -18,10 +18,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 #Add email_sending configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'youme@gmail.com'
 EMAIL_HOST_USER = 'pil1.youme@gmail.com'
 EMAIL_HOST_PASSWORD = '20077'
 
--- Youme/urls.py
+++ Youme/urls.py
@@ -1,6 +1,7 @@
 # from django.conf.urls import include
 from .views import *
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     ##################### AUTHENTIFICATION #################################
@@ -8,6 +9,10 @@ urlpatterns = [
     path('', accueil, name= 'accueil'),
     path('inscription/', inscription, name= 'inscription'),
     path('connexion/', connexion, name= 'connexion'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name = 'password_reset'),
+	path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('deconnexion/', deconnexion, name= 'deconnexion'),
     ######################## SUGGESTIONS ET PROFILS #########################
     # path('view_profiles/', view_profiles, name='view_profiles'),
